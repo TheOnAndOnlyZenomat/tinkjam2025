ae8438fa625e3202d6b4a146c4e20304fc62d638
zeno <tmp-git@groundzeno.net>
2025-03-22 02:04:33 +0100

    MapManager: implement hover highlight

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class MapManager : MonoBehaviour
 {
@@ -8,16 +7,16 @@ public class MapManager : MonoBehaviour
 	private Region[] regions;
 	private Region activeRegion = null;
 
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
 			Debug.Log("MapManager instance was not me, destroying");
-            Destroy(this.gameObject);
-        } else {
-            Instance = this;
-        }
-    }
+			Destroy(this.gameObject);
+		} else {
+			Instance = this;
+		}
+	}
 
 	void Start()
 	{
@@ -57,33 +56,15 @@ public class MapManager : MonoBehaviour
 		this.regions[index].ApplyStatus(status);
 	}
 
-	public void OnMousePress() {
-		// get mouse click's position in 2d plane
-		Vector3 mouse_world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		mouse_world.z = 0;
-
-		Debug.Log($"Mouse clicked at {{x: {mouse_world.x}; y: {mouse_world.y}}}");
-
-		Grid grid = this.regions[0].grid;
-		Tilemap tilemap = grid.transform.Find("Tilemap").GetComponent<Tilemap>();
-		Vector3Int cellCoords = tilemap.WorldToCell(mouse_world);
-
-		tilemap.SetTileFlags(cellCoords, TileFlags.None);
-		tilemap.SetColor(cellCoords, Color.red);
-	}
-
 	public void SetActiveRegion(Region region) {
-		this.activeRegion = region;
-
 		if (region is null) {
-			Debug.Log("Unsetting active region");
+			this.activeRegion.Highlight(false);
+			this.activeRegion = region;
+
 			return;
 		}
 
-		int index = -1;
-		for (int i = 0; i < this.regions.Length; i++) {
-			if (this.regions[i] == region) index = i;
-		}
-		Debug.Log($"Active region is {index}");
+		this.activeRegion = region;
+		this.activeRegion.Highlight(true);
 	}
 }
