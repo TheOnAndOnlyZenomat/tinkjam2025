20a35fe512b0d78d59ec6a100e9205e848670151
zeno <tmp-git@groundzeno.net>
2025-03-23 11:35:23 +0100

    Cards: add level 4 cards

--- Assets/Scripts/CardStatuse/HagelsturmStatus.cs
+++ Assets/Scripts/CardStatuse/HagelsturmStatus.cs
@@ -4,8 +4,12 @@ public class HagelsturmStatus : Status
 {
 	public override string statusName { get; } = "Hagelsturm";
 
+	public int damage = 75;
+
 	public override void ApplyDamageToEnemy(GameObject enemy) {
 		Debug.Log("Hagelsturm is applying damage");
 		AudioManager.Instance.PlayHagelsturm();
+
+		enemy.GetComponent<EnemyStats>().Damage(this.damage);
 	}
 }
--- Assets/Scripts/CardStatuse/ResetStatus.cs
+++ Assets/Scripts/CardStatuse/ResetStatus.cs
@@ -4,8 +4,12 @@ public class ResetStatus : Status
 {
 	public override string statusName { get; } = "Reset";
 
+	public float timerBonus = 120f;
+
 	public override void ApplyDamageToEnemy(GameObject enemy) {
 		Debug.Log("Reset is applying damage");
 		AudioManager.Instance.PlayReset();
+
+		TimerScript.Instance.SetTime(timerBonus);
 	}
 }
